ea98283ec04647bb68ad033075709b78abc22e62
quentinrouger <quentin_rouger@hotmail.fr>
2024-04-18 10:40:07 +0200

    Refactor dashboard click event handling

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -9,12 +9,9 @@ $('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
 
-  // Update counter function
-  const updateCounter = _.debounce(() => {
-    count++;
-    const clicksText = count === 1 ? 'click' : 'clicks';
-    $('#count').text(`${count} ${clicksText} on the button`);
-  }, 500);
+$('button').on("click", _.debounce(updateCounter, 500))
 
-  // Bind updateCounter to button click event
-  $('button').on('click', updateCounter);
+function updateCounter() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+}
