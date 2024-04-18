d198736948766b02bcc259b37df37115fcbca59d
quentinrouger <quentin_rouger@hotmail.fr>
2024-04-18 12:41:14 +0200

    Add initial webpack setup with dashboard main page and assets

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './js/dashboard_main.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|jpg|jpe?g|svg)$/i,
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            type: 'asset/resource',
+                        },
+                    }
+                ]
+      },
+    ],
+  },
+};
