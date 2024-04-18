53052daacae22f7445ed9a06b6910f9aa00b3b4c
quentinrouger <quentin_rouger@hotmail.fr>
2024-04-18 09:58:44 +0200

    Task 1 : Add initial dashboard setup with jQuery, lodash, and webpack configuration

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(() => {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  let count = 0;
+
+    // Update counter function
+    const updateCounter = _.debounce(() => {
+      count++;
+      $('#count').text(`${count} clicks on the button`);
+    }, 500);
+
+    $('button').on('click', () => {
+      count++;
+      $('#count').text(`${count} clicks on the button`);
+    });
+});
