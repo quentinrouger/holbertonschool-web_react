bfc7890540bf03df900f4a55128447809177fbb6
quentinrouger <quentin_rouger@hotmail.fr>
2024-04-18 10:10:37 +0200

    Refactor dashboard_main.js to remove document.ready wrapper

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,22 +1,20 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-$(document).ready(() => {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-  let count = 0;
+let count = 0;
 
-    // Update counter function
-    const updateCounter = _.debounce(() => {
-      count++;
-      const clicksText = count === 1 ? 'click' : 'clicks';
-  $('#count').text(`${count} ${clicksText} on the button`);
+  // Update counter function
+  const updateCounter = _.debounce(() => {
+    count++;
+    const clicksText = count === 1 ? 'click' : 'clicks';
+$('#count').text(`${count} ${clicksText} on the button`);
 }, 500);
 
-    // Bind updateCounter to button click event
-    $('button').on('click', updateCounter);
-});
+  // Bind updateCounter to button click event
+  $('button').on('click', updateCounter);
