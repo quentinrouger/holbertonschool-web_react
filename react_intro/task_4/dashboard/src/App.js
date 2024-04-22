e9c3bd34fff253f756ff2b5e5c81d9e3af44b47e
quentinrouger <quentin_rouger@hotmail.fr>
2024-04-22 17:43:14 +0200

    Add initial dashboard setup with React, including public assets, package configuration, main App component, Notifications component, utility functions, tests, and styling

---
+++ react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import logo from './holberton_logo.jpeg';
+import { getFullYear, getFooterCopy } from './utils';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src= {logo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" style={{ marginRight: '10px' }}/>
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" style={{ marginRight: '10px' }}/>
+        <button type='button'>OK</button>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
