4d5448d528e36d73cbfdefe4b805953a8980251b
quentinrouger <quentin_rouger@hotmail.fr>
2024-06-05 11:12:15 +0200

    Modify the index.js to support the Reduc DevTools extension

--- react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,18 +1,16 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App/App';
 import { uiReducer } from './reducers/uiReducer';
 import { thunk } from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 // Create the Redux store
-const store = createStore(
-  uiReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(uiReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
